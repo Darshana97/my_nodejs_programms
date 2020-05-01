72a0233a88756a5c3a3c364af4f85a4a44f0aa5c
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-05-01 17:22:31 +0530

    template engine

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const path = require("path");
 const expressEdge = require("express-edge");
 
 const express = require("express");
+// const bodyParser = require("body-parser");
 
 const app = express();
 
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(expressEdge);
+
+
 
 app.set("views", `${__dirname}/views`);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
