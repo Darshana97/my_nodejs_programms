cd494b4ce6b1fa8125869cf92eb99c265bfd7dab
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-05-01 15:54:57 +0530

    create home page

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const path = require("path");
+
 const express = require("express");
 
 const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
